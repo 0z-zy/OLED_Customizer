ea92bfca45bdaa1c728763d9540f16b7d894a2e9
SSnowy9 <super_boy_2011@hotmail.com>
2026-04-08 20:56:28 +0300

    Extension extra port

--- extension/background.js
+++ extension/background.js
@@ -1,12 +1,17 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.action === 'sendMediaData') {
         const data = message.data;
+        const body = JSON.stringify(data);
+        const headers = { 'Content-Type': 'application/json' };
+
+        // Send to OLED Customizer (port 1231, as set in config.json discord_local_port)
         fetch('http://127.0.0.1:1231/extension_data', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(data)
-        }).catch(e => {
-            // Silently fail if app isn't running
-        });
+            method: 'POST', headers, body
+        }).catch(() => { /* app not running, ignore */ });
+
+        // Send to additional app (port 2409)
+        fetch('http://127.0.0.1:2409/extension_data', {
+            method: 'POST', headers, body
+        }).catch(() => { /* app not running, ignore */ });
     }
 });
